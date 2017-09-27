95a17763abb43232fbffa0eb5fdd91797be8d808
leehave <1831553725@qq.com>
2017-09-27 17:18:41 +0800

    update

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,3 +1,179 @@
 <template>
-  <p>rank</p>
+  <div class="mod_topic current_page loaded rank" ref="rank">
+    <scroll :data="topList" class="toplist" ref="toplist">
+      <ul>
+        <li @click="selectItem(item)" class="item" v-for="item in topList">
+          <div class="topic_media">
+            <img width="100" height="100" v-lazy="item.picUrl"/>
+            <span class="listen_count">
+              <i class="icon icon_listen"></i>{{item.listenCount|playcount}}
+            </span>
+          </div>
+          <div class="songlist">
+            <h3 class="topic_tit">{{item.topTitle}}</h3>
+            <div class="song" v-for="(song,index) in item.songList">
+              <p>{{index + 1}}<span class="text_name">{{song.songname}}</span>-{{song.singername}}</p>
+            </div>
+            <i class="topic_arrow"></i>
+          </div>
+        </li>
+      </ul>
+      <div class="loading-container" v-show="!topList.length">
+        <loading></loading>
+      </div>
+    </scroll>
+  </div>
 </template>
+
+<script type="text/ecmascript-6">
+  import Scroll from '../../base/scroll/scroll'
+  import Loading from '../../base/loading/loading'
+  import {getTopList} from '../../api/rank'
+  import {ERR_OK} from '../../api/config'
+  import {playlistMixin} from '../../common/js/mixin'
+  import {mapMutations} from 'vuex'
+  export default {
+    created() {
+      this._getTopList()
+    },
+    data() {
+      return {
+        topList: []
+      }
+    },
+    methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+
+        this.$refs.rank.style.bottom = bottom
+        this.$refs.toplist.refresh()
+      },
+      selectItem(item) {
+        this.$router.push({
+          path: `/rank/${item.id}`
+        })
+        this.setTopList(item)
+      },
+      _getTopList() {
+        getTopList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.topList = res.data.topList
+          }
+        })
+      },
+      ...mapMutations({
+        setTopList: 'SET_TOP_LIST'
+      })
+    },
+    filters: {
+			playcount(v) {
+				if(!v) return "0";
+				return v < 10e3 ? v : ((v / 10e3).toFixed(1) + '万')
+			}
+		},
+    watch: {
+      topList() {
+        setTimeout(() => {
+          this.$Lazyload.lazyLoadHandler()
+        }, 20)
+      }
+    },
+    components: {
+      Scroll,
+      Loading
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/minxin"
+
+  .rank
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .toplist
+      height: 100%
+      overflow: hidden
+      .item
+        display: flex
+        margin: 0 10px
+        padding-top: 10px
+        height: 100px
+        &:last-child
+          padding-bottom: 20px
+        .topic_media
+          flex: 0 0 100px
+          width: 100px
+          height: 100px
+          position: relative
+          .listen_count
+            left: 5px
+            bottom: 7px
+            line-height: 12px
+            color: #fff
+            opacity: .6
+            font-size: 9px
+            z-index: 10
+            position: absolute
+            display: block
+        .icon_listen
+          display: block;
+          float: left
+          width: 10px
+          height: 10px
+          background-position: 0 -50px
+          margin-right: 3px
+        .topic_tit
+          font-size: 16px
+          color: $color-normal
+          font-weight: normal
+          margin-bottom: 5px
+          line-height: 1.5
+        .icon
+          background-image: url(../../common/image/list_sprite.png)
+          background-repeat: no-repeat
+          background-size: 24px 60px
+          z-index: 10
+        .songlist
+          flex: 1
+          display: flex
+          flex-direction: column
+          justify-content: center
+          padding: 0 10px 0 15px
+          height: 100px
+          overflow: hidden
+          background: $color-white
+          color: $color-normal
+          font-size: $font-size-small
+          position: relative
+          -webkit-box-align: center
+          -webkit-box-pack: center
+          .text_name
+             color: #000
+             margin-left: 8px
+             margin-right: 5px
+             font-style: normal
+         .topic_arrow
+           position: absolute
+           right: 10px
+           top: 50%
+           margin-top: -4px
+           width: 6px
+           height: 6px
+           border-right: 1px solid #b2b2b2
+           border-bottom: 1px solid #b2b2b2
+           -webkit-transform: rotate(-45deg)
+          .song p
+            no-wrap()
+            line-height: 20px
+            font-size: $font-size-medium
+            color: rgba(0,0,0,.5)
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,86 +1,88 @@
 <template>
-  <div class="recommend" ref="recommend">
-      <div class="slider-container">
-        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
-          <slider>
-            <div v-for="item in recommends">
-              <a :href="item.linkUrl">
-                <img class="needsclick" @load="loadImage" :src="item.picUrl">
-              </a>
-            </div>
-          </slider>
-        </div>
+  <div>
+    <div class="recommend" ref="recommend">
+    <div class="slider-container">
+      <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img class="needsclick" @load="loadImage" :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
       </div>
+    </div>
+    <div class="recommend-list mod_twocol_list mod_twocol_list_normal">
+      <h2 class="list_title">电台</h2>
+      <ul class="list_container">
+        <li v-for="item in recomradio" class="item">
+          <a href="" class="list_main">
+            <div class="list_media">
+              <img :src="item.picUrl">
+              <span class="icon icon_play"></span>
+            </div>
+            <div class="list_info">
+              <h2 class="list_tit tit_two_row" v-html="item.Ftitle"></h2>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="mod_footer">
+    <div class="footer_logo">QQ音乐</div>
+    <p class="copyright">Copyright © 1998 - <span id="js_cpright_year">2017</span> Tencent. All Rights Reserved.</p>
+    <p id="js_iosinfo" class="copyright" style="">联系电话：0755-86013388 QQ群：55209235</p>
+  </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import Slider from '../../base/slider/slider'
-  import Loading from '../../base/loading/loading'
-  import {getRecommend} from '../../api/recommend'
-  // import {playlistMixin} from '../../common/js/mixin'
-  import {ERR_OK} from '../../api/config'
-  import {mapMutations} from 'vuex'
+import Slider from '../../base/slider/slider'
+import Loading from '../../base/loading/loading'
+import { getRecommend } from '../../api/recommend'
+import { ERR_OK } from '../../api/config'
+import { mapMutations } from 'vuex'
 
-  export default {
-    // mixins: [playlistMixin],
-    data(){
-      return {
-        recommends: []
-        // discList: []
+export default {
+  data() {
+    return {
+      recommends: [],
+      recomradio: []
+    }
+  },
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    loadImage() {
+      if (!this.checkloaded) {
+        this.checkloaded = true
       }
     },
-    created(){
-      this._getRecommend()
-
-      // this._getDiscList()
-    },
-    methods: {
-      loadImage(){
-        if (!this.checkloaded) {
-          this.checkloaded = true
+    _getRecommend() {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider,
+            this.recomradio = res.data.radioList
         }
-      },
-      // selectItem(item){
-      //   this.$router.push({
-      //     path: `/recommend/${item.dissid}`
-      //   })
-      //   this.setDisc(item)
-      // },
-      _getRecommend(){
-        getRecommend().then((res) => {
-          if (res.code === ERR_OK) {
-            this.recommends = res.data.slider
-          }
-        })
-      },
-      // _getDiscList(){
-      //   getDiscList().then((res) => {
-      //     if (res.code === ERR_OK) {
-      //       this.discList = res.data.list
-      //     }
-      //   })
-      // },
-      // ...mapMutations({
-      //   setDisc: 'SET_DISC'
-      // })
-    },
-    components: {
-      Slider,
-      Loading
+      })
     }
+  },
+  components: {
+    Slider,
+    Loading
   }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
+  @import "~common/stylus/minxin.styl";
   .slider-container
     position: relative
+    margin-bottom: 14px
   .recommend
-    position: fixed
-    width: 100%
-    top: 88px
-    bottom: 0
     .recommend-content
       height: 100%
       overflow: hidden
@@ -88,38 +90,90 @@
         position: relative
         width: 100%
         overflow: hidden
-      .recommend-list
-        .list-title
-          height: 65px
-          line-height: 65px
-          text-align: center
-          font-size: $font-size-medium
-          color: $color-theme
-        .item
-          display: flex
-          box-sizing: border-box
-          align-items: center
-          padding: 0 20px 20px 20px
-          .icon
-            flex: 0 0 60px
-            width: 60px
-            padding-right: 20px
-          .text
-            display: flex
-            flex-direction: column
-            justify-content: center
-            flex: 1
-            line-height: 20px
-            overflow: hidden
-            font-size: $font-size-medium
-            .name
-              margin-bottom: 10px
-              color: $color-text
-            .desc
-              color: $color-text-d
       .loading-container
         position: absolute
         width: 100%
         top: 50%
         transform: translateY(-50%)
+  .mod_twocol_list_normal
+    margin-bottom: 7px
+  .mod_twocol_list
+    margin-left: 10px;
+    margin-right: 10px
+    .list_title
+      font-size: 16px;
+      color: $color-normal;
+      margin-bottom: 11px;
+      font-weight: normal
+    .list_container
+      overflow: hidden;
+      margin-right: -8px
+    li
+      float: left;
+      width: 50%;
+      -webkit-box-sizing: border-box;
+      padding-right: 8px;
+      margin-bottom: 10px;
+      overflow: hidden
+      .list_main
+        display: block;
+        background: #fff
+      .list_info
+        padding: 0 7px 5px;
+        color: $color-normal
+      .list_media
+        position: relative;
+        margin-bottom: 5px
+        &::before
+          content: "";
+          display: block;
+          padding-top: 100%
+        .icon_play
+          height: 24px
+          bottom: 5px
+          right: 5px
+          width: 24px
+          background-position: 0 0
+          position: absolute
+          display: block
+          z-index: 10
+          background-image: url(../../common/image/list_sprite.png);
+          background-repeat: no-repeat;
+          background-size: 24px 60px
+      .list_tit,.list_text
+        white-space: nowrap
+        overflow: hidden
+        text-overflow: ellipsis
+        font-weight: normal
+        font-size: $font-size-small
+        height: 36px;
+        line-height: 18px
+        white-space: normal
+        word-wrap: break-word
+        font-size :$font-size-medium
+      img
+        position: absolute
+        top: 0
+        left: 0
+        display: block
+        width: 100%
+        z-index: 1
+  .mod_footer
+    padding: 15px 0
+    .footer_logo
+      display: block
+      width: 82px
+      height: 24px
+      margin: 0 auto 10px
+      background-image: url(../../common/image/logo_footer.png)
+      background-repeat: no-repeat
+      background-size: cover
+      text-indent: -9999px
+    .copyright
+      font-size: 10px
+      text-align: center
+      color: $color-normal
+      font-style:normal
+      line-height: 1.5
+
 </style>
